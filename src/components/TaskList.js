4197e2f89dd71b1a202e3ff6934b9187d9ba0593
unknown <ranaete14@gmail.com>
2023-04-18 09:55:50 -0700

    yarn build successfull

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -14,20 +14,35 @@ export default function TaskList({ category, tasks, setAllTasks }) {
         >
             <div className="tasklist-header">
                 <h2 className="tasklist-title">{category}</h2>
+            </div>
+            <div className="tasklist-list">
+                {tasks.map((task) => {
+                    return <TaskBlock key={task.id} task={task} />;
+                })}
                 {category === "Trash" && (
-                    <div>
+                    <div
+                        style={{
+                            display: "flex",
+                            flexDirection: "row",
+                            justifyContent: "center",
+                        }}
+                    >
                         <button
+                            style={{
+                                backgroundColor: "#ff4041",
+                                alignSelf: "center",
+                                padding: 5,
+                                width: 120,
+                                border: "none",
+                                borderRadius: 5,
+                                color: "white",
+                            }}
                             onClick={() => taskCrud.clearTrash(setAllTasks)}
                         >
                             Clear
                         </button>
                     </div>
                 )}
-            </div>
-            <div className="tasklist-list">
-                {tasks.map((task) => {
-                    return <TaskBlock key={task.id} task={task} />;
-                })}
             </div>
         </div>
     );
