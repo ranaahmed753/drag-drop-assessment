97341c4ba336c0a8675ece443c5157b6512b7912
unknown <ranaete14@gmail.com>
2023-04-18 08:00:56 -0700

    task utilities such as doing-task,done-task,trash-task has been added

---
+++ feature/taskUtils.js
@@ -0,0 +1,17 @@
+const categorizeTasks = (allTasks) => {
+    const noCatTasks = allTasks.filter((task) => task.status === "");
+    const doingTasks = allTasks.filter((task) => task.status === "doing");
+    const doneTasks = allTasks.filter((task) => task.status === "done");
+    const trashTasks = allTasks.filter((task) => task.status === "trash");
+
+    return {
+        noCatTasks,
+        doingTasks,
+        doneTasks,
+        trashTasks,
+    };
+};
+
+export default {
+    categorizeTasks,
+};
