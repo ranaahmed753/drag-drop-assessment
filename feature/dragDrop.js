7d768e3b4f07fb9a9ed8555a8cfc9cf552ea9eef
unknown <ranaete14@gmail.com>
2023-04-18 08:06:18 -0700

    drag-drop functionality like dragStart,drag-over,handleDrop has been used

---
+++ feature/dragDrop.js
@@ -0,0 +1,27 @@
+import taskCrud from "./taskCrud";
+
+const handleDragStart = (event, taskId) => {
+    event.dataTransfer.setData("text/plain", taskId.toString());
+};
+
+const handleDragOver = (event, category) => {
+    event.preventDefault();
+    console.log(category);
+};
+
+const handleDrop = (event, setAllTasks, category) => {
+    event.preventDefault();
+    const taskId = event.dataTransfer.getData("text/plain");
+    const targetCategory = category.toLowerCase();
+    taskCrud.updateTask(
+        parseInt(taskId),
+        { status: targetCategory === "todo" ? "" : targetCategory },
+        setAllTasks
+    );
+};
+
+export default {
+    handleDragStart,
+    handleDragOver,
+    handleDrop,
+};
